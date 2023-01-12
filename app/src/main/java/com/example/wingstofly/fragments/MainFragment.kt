c6cbf1a7425b56e35cc8d3d64154c4dc7a799264
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-12 06:01:35 +0300

    set up the navigation view with a nav controller.

---
+++ app/src/main/java/com/example/wingstofly/fragments/MainFragment.kt
@@ -0,0 +1,50 @@
+package com.example.wingstofly.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.GravityCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.wingstofly.R
+import com.example.wingstofly.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment(), View.OnClickListener {
+    private lateinit var bind:FragmentMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        bind = FragmentMainBinding.inflate(layoutInflater)
+        val navHostFragment = childFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        bind.navView.bringToFront()
+        bind.navView.setupWithNavController(navHostFragment.navController)
+        return bind.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MainFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+
+    override fun onClick(p0: View?) {
+        if (p0 == bind.image){
+            bind.drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+}
