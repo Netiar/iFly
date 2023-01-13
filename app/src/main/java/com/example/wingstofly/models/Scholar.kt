d3cd341fed6a73a5e86f88101e90728096bde0e7
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-13 23:19:10 +0300

    add a primary constructor and member variables

---
+++ app/src/main/java/com/example/wingstofly/models/Scholar.kt
@@ -0,0 +1,9 @@
+package com.example.wingstofly.models
+
+
+class Scholar(private val name:String, private val status:String ) {
+    private lateinit var origin:String
+    private var id:Int? = 0
+
+
+}
