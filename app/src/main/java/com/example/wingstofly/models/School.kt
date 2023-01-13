45dafe9108988131cc26d8affe4d032d30cd6c60
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-13 23:21:43 +0300

    add a primary constructor and member variables

---
+++ app/src/main/java/com/example/wingstofly/models/School.kt
@@ -0,0 +1,10 @@
+package com.example.wingstofly.models
+
+import kotlin.properties.Delegates
+
+class School(private val name:String, private val location: String) {
+    private var website:String? = null
+    private var studentId:Int? = null
+
+
+}
