9ba9d6912b933d8401e82463151ac11945a9c997
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-13 23:21:08 +0300

    add getters and setters

--- app/src/main/java/com/example/wingstofly/models/Scholar.kt
+++ app/src/main/java/com/example/wingstofly/models/Scholar.kt
@@ -5,5 +5,15 @@ class Scholar(private val name:String, private val status:String ) {
     private lateinit var origin:String
     private var id:Int? = 0
 
+    fun getId() = this.id
+    fun getOrigin() = this.origin
+
+    fun setOrigin(origin: String){
+        this.origin = origin
+    }
+
+    fun setId(id:Int){
+        this.id = id
+    }
 
 }
