2b56809ac1704b573885bb7402f60b1a810729ca
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-13 23:22:11 +0300

    add getters and setters

--- app/src/main/java/com/example/wingstofly/models/School.kt
+++ app/src/main/java/com/example/wingstofly/models/School.kt
@@ -6,5 +6,16 @@ class School(private val name:String, private val location: String) {
     private var website:String? = null
     private var studentId:Int? = null
 
+    fun getStudentId() = this.studentId
+    fun getWebsite() = this.website
+    fun getName() = this.name
+    fun getLocation() = this.location
 
+    fun setWebsite(website:String){
+        this.website = website
+    }
+
+    fun setStudentId(id:Int){
+        this.studentId = id
+    }
 }
