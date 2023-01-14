ecd8cfe413ea51f3cb2ca9f3d3049ed44706d3ad
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-14 23:21:34 +0300

    add a primary constructor

--- app/src/main/java/com/example/wingstofly/adapters/ScholarsRecAdapter.kt
+++ app/src/main/java/com/example/wingstofly/adapters/ScholarsRecAdapter.kt
@@ -2,15 +2,20 @@ package com.example.wingstofly.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.findFragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
 import com.example.wingstofly.R
+import com.example.wingstofly.databinding.ChatBinding
+import com.example.wingstofly.databinding.FragmentScholarBinding
 import com.example.wingstofly.databinding.ScholarBinding
+import com.example.wingstofly.databinding.ScholarSuggestionBinding
 import com.example.wingstofly.fragments.ScholarFragment
 
-class ScholarsRecAdapter: RecyclerView.Adapter<ScholarsRecAdapter.MyHolder>() {
-    inner class MyHolder(private val bind: ScholarBinding): RecyclerView.ViewHolder(bind.root) {
+class ScholarsRecAdapter(private var recBind: ViewBinding?, private var mainBind:FragmentScholarBinding): RecyclerView.Adapter<ScholarsRecAdapter.MyHolder>() {
+    inner class MyHolder(bind: ViewBinding): RecyclerView.ViewHolder(bind.root) {
         init {
             bind.root.setOnClickListener{
                 it.findFragment<ScholarFragment>().findNavController().navigate(R.id.action_scholarFragment2_to_singleScholarFragment)
@@ -19,8 +24,15 @@ class ScholarsRecAdapter: RecyclerView.Adapter<ScholarsRecAdapter.MyHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
-        val v = ScholarBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MyHolder(v)
+        recBind = if (parent == mainBind.myRecView){
+            Toast.makeText(parent.context, "${parent.toString()}", Toast.LENGTH_LONG).show()
+            ScholarBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        }else{
+            Toast.makeText(parent.context,
+                parent.toString(), Toast.LENGTH_LONG).show()
+            ScholarSuggestionBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        }
+        return MyHolder(recBind!!)
     }
 
     override fun onBindViewHolder(holder: MyHolder, position: Int) {
