3c3fec17c5ed5d830b6bf5415ca14d84cf4f8ff5
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-19 08:26:56 +0300

    define a method to get questions fro the api

--- app/src/main/java/com/example/wingstofly/api/Topics.kt
+++ app/src/main/java/com/example/wingstofly/api/Topics.kt
@@ -1,10 +1,20 @@
 package com.example.wingstofly.api
 
+import com.example.wingstofly.models.Topic
 import com.example.wingstofly.models.TopicCategory
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface Topics {
     @GET("api_category.php")
     suspend fun getCategories():Response<TopicCategory>
+
+    @GET("api.php")
+    suspend fun getQuetsions(
+        @Query("amount") amount: String,
+        @Query("category")category:String,
+        @Query("difficulty") difficulty:String,
+        @Query("type") type: String
+    ): Response<Topic>
 }
