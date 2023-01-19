954bd51e7e9d94129fc448aaead88d22b7710430
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-19 19:28:41 +0300

    inflate the view to be used by the recycler view.

---
+++ app/src/main/java/com/example/wingstofly/adapters/ChatRecHolder.kt
@@ -0,0 +1,30 @@
+package com.example.wingstofly.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wingstofly.R
+
+class ChatRecHolder: RecyclerView.Adapter<ChatRecHolder.MyHolder>() {
+    inner class MyHolder(view: View): RecyclerView.ViewHolder(view){
+
+    }
+
+//    private var diffUtil:
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = MyHolder(
+        LayoutInflater.from(parent.context).inflate(
+            R.layout.chat,
+            parent,
+            false
+        )
+    )
+}
