fe4a2cfad264f47ae465a7d849ea8618bedf79b7
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-19 08:30:13 +0300

    initialise a view model, repository and view model provider instance.

--- app/src/main/java/com/example/wingstofly/MainActivity.kt
+++ app/src/main/java/com/example/wingstofly/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.wingstofly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.wingstofly.repository.QuizRepository
+import com.example.wingstofly.viewmodel.QuizProviderFactory
+import com.example.wingstofly.viewmodel.QuizViewModel
 
 class MainActivity : AppCompatActivity() {
+    lateinit var questViewModel: QuizViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val repo = QuizRepository()
+        val provider = QuizProviderFactory(repo)
+        questViewModel = ViewModelProvider(this, provider)[QuizViewModel::class.java]
     }
 }
