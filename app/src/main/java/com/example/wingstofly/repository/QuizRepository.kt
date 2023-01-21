b08959d8256dfc301cbffdf820b63f27bdc496a9
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-21 22:50:03 +0300

    add a method to get the quiz topics from the api.

--- app/src/main/java/com/example/wingstofly/repository/QuizRepository.kt
+++ app/src/main/java/com/example/wingstofly/repository/QuizRepository.kt
@@ -1,12 +1,17 @@
 package com.example.wingstofly.repository
 
 import com.example.wingstofly.api.RetrofitClient
+import com.example.wingstofly.database.DataManager
 import com.example.wingstofly.models.Topic
 import retrofit2.Response
 
 class QuizRepository {
+    private val dataManager = DataManager()
+
+    suspend fun getTopics() = RetrofitClient.getApiClient.getCategories()
 
     suspend fun getQuestions(amount: String = "10", difficulty: String = "hard", type: String = "multiple", category: String = "any") =
         RetrofitClient.getApiClient.getQuetsions(amount, category, difficulty, type)
+
 
 }
