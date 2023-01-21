6c9545f53d6f117ff78b264e0d5ea662b0c147bc
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-21 22:50:54 +0300

    create a method to post the state of the request to the observers.

--- app/src/main/java/com/example/wingstofly/viewmodel/QuizViewModel.kt
+++ app/src/main/java/com/example/wingstofly/viewmodel/QuizViewModel.kt
@@ -4,14 +4,39 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.wingstofly.models.Topic
+import com.example.wingstofly.models.TopicCategory
 import com.example.wingstofly.repository.QuizRepository
 import com.example.wingstofly.utils.Resource
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
 class QuizViewModel(private val quizRepository: QuizRepository): ViewModel() {
+    val topics:MutableLiveData<Resource<TopicCategory>> = MutableLiveData()
     val questions: MutableLiveData<Resource<Topic>> = MutableLiveData() //posts api request state to the observer
     val amount: String = "10" ;val difficulty: String = "hard"; val type: String = "multiple"; val category: String = "any"
+
+    init {
+        getQuestions()
+        getCategories()
+    }
+
+    private fun getCategories() = viewModelScope.launch {
+        topics.postValue(Resource.Loading())
+        val response = quizRepository.getTopics()
+        topics.postValue(getTopicsState(response))
+    }
+
+    private fun getTopicsState(response: Response<TopicCategory>): Resource<TopicCategory>{ //getting the requests current state
+        if(response.isSuccessful){
+            response.body()?.let { topicCategory ->
+                return Resource.Success(topicCategory)
+            }
+        }
+        return Resource.Failure(message = response.message())
+
+    }
+
+
 
 
     private fun getQuestions() = viewModelScope.launch {
