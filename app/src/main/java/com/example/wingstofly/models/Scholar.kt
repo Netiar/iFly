ab4125d4e423c62d71d973309b84a6ad4ff59801
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-21 22:48:46 +0300

    add variables to hold scholars educational info

--- app/src/main/java/com/example/wingstofly/models/Scholar.kt
+++ app/src/main/java/com/example/wingstofly/models/Scholar.kt
@@ -4,8 +4,13 @@ package com.example.wingstofly.models
 class Scholar(private val name:String, private val status:String ) {
     private lateinit var origin:String
     private var id:Int? = 0
+    var primarySchool: String? = null
+    var secondarySchool: String? = null
+    var varsity: String? = null
 
     fun getId() = this.id
+    fun getName() = this.name
+    fun getStatus() = this.status
     fun getOrigin() = this.origin
 
     fun setOrigin(origin: String){
