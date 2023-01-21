6a3de53638e047420ab4a73918cf667ed2065e92
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-21 22:54:56 +0300

    create a class to represent a single event instance.

---
+++ app/src/main/java/com/example/wingstofly/models/Event.kt
@@ -0,0 +1,10 @@
+package com.example.wingstofly.models
+
+import java.util.Date
+
+class Event(var title:String){
+    var description:String? = null
+    var venue:String? = null
+    var date:Date? = null
+}
+
