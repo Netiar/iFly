ba3f8cb5a85a9b479a6b00fc5354d8624ed5dbe5
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-22 18:40:43 +0300

    add a method to validate input text.

---
+++ app/src/main/java/com/example/wingstofly/utils/Validator.kt
@@ -0,0 +1,21 @@
+package com.example.wingstofly.utils
+
+import com.google.android.material.textfield.TextInputLayout
+
+class Validator {
+    fun validateInputText(input: TextInputLayout): Boolean{
+        return if (input.editText?.text?.isBlank() == true){
+            input.apply {
+                isErrorEnabled = true
+                error = "This field cannot be empty"
+            }
+            false
+
+        }else{
+            input.apply {
+                isErrorEnabled = false
+            }
+            true
+        }
+    }
+}
