6b514a32a7aa8e667ed7bc08b99000dd921c9cdb
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-22 18:38:24 +0300

    add a method to validate the scholars pf number.

--- app/src/main/java/com/example/wingstofly/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/wingstofly/fragments/LoginFragment.kt
@@ -8,44 +8,56 @@ import android.view.ViewGroup
 import androidx.fragment.app.findFragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import com.example.wingstofly.MainActivity
 import com.example.wingstofly.R
-import com.example.wingstofly.databinding.FragmentLoginBinding
+import com.example.wingstofly.databinding.FragmentSignBinding
+import com.example.wingstofly.models.Scholar
+import com.example.wingstofly.utils.Validator
+import kotlinx.android.synthetic.main.fragment_sign.*
 
 
 class LoginFragment : Fragment(), View.OnClickListener {
-    private lateinit var bind:FragmentLoginBinding
+    private lateinit var bind:FragmentSignBinding
+    private lateinit var scholars: ArrayList<Scholar>
+    private lateinit var scholar: Scholar
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        bind = FragmentLoginBinding.inflate(layoutInflater)
+        bind = FragmentSignBinding.inflate(layoutInflater)
+
+        scholars = (activity as MainActivity).scholars
+
         bind.submit.setOnClickListener(this::onClick)
+
+
 
         return bind.root
     }
 
-    companion object {
-      @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                }
-            }
-    }
-
     override fun onClick(p0: View?) {
         if (p0 == bind.submit){
-            p0.findFragment<LoginFragment>().findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
-//            val navHostFragment = childFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-//            navHostFragment.navController.navigate(R.id.action_loginFragment_to_mainFragment)
+            validatePfNumber(p0)
+        }
+    }
+
+    private fun validatePfNumber(p0: View?){
+
+            if (!Validator().validateInputText(scholar_id)){
+                return
+            }
+        val pfNumber = bind.scholarId.editText!!.text.trim()
+
+        for (i in 0 until scholars.size){
+            val currentPfNumber = scholars[i].pfNumber
+            if (pfNumber.contentEquals(currentPfNumber, true)){
+                scholar = scholars[i]
+                p0?.findFragment<LoginFragment>()?.findNavController()?.navigate(R.id.action_loginFragment_to_mainFragment)
+                break
+            }else{
+                scholar_id.error = "Enter the correct number"
+            }
         }
     }
 }
