4fc04c59a569f29df638eb6021b1fb87755782c9
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-22 18:39:17 +0300

    add a member variable to store the scholars pf number.

--- app/src/main/java/com/example/wingstofly/models/Scholar.kt
+++ app/src/main/java/com/example/wingstofly/models/Scholar.kt
@@ -3,22 +3,17 @@ package com.example.wingstofly.models
 
 class Scholar(private val name:String, private val status:String ) {
     private lateinit var origin:String
-    private var id:Int? = 0
+    var pfNumber: String = ""
     var primarySchool: String? = null
     var secondarySchool: String? = null
     var varsity: String? = null
 
-    fun getId() = this.id
     fun getName() = this.name
     fun getStatus() = this.status
     fun getOrigin() = this.origin
 
     fun setOrigin(origin: String){
         this.origin = origin
-    }
-
-    fun setId(id:Int){
-        this.id = id
     }
 
 }
