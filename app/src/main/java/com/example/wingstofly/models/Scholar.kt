e2db8fd93f991920eece4aa3ec1e33dda85f9f08
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-22 18:43:33 +0300

    define a method to assign a grade to a subject.

--- app/src/main/java/com/example/wingstofly/models/Scholar.kt
+++ app/src/main/java/com/example/wingstofly/models/Scholar.kt
@@ -22,6 +22,46 @@ class Scholar(private val name:String, private val status:String ) {
         currentSubjects.add(subject)
         calculateMeanScore()
     }
+    private fun assignGrades(marks: Int): String{
+
+        agpValues["A"] = 12
+        agpValues["A-"] = 11
+        agpValues["B+"] = 10
+        agpValues["B"] = 9
+        agpValues["B-"] = 8
+        agpValues["C+"] = 7
+        agpValues["C"] = 6
+        agpValues["C-"] = 5
+        agpValues["D+"] = 4
+        agpValues["D"] = 3
+        agpValues["D-"] = 2
+        agpValues["E"] = 1
+
+        if(marks >= 79){
+            return "A"
+        }else if (marks in 75..78){
+            return "A-"
+        }else if (marks in 70..74){
+            return "B+"
+        }else if (marks in 65..69){
+            return "B"
+        }else if (marks in 60..64){
+            return "B-"
+        }else if (marks in 55..59){
+            return "C+"
+        }else if (marks in 45..54){
+            return "C"
+        }else if (marks in 40..44){
+            return "C-"
+        }else if (marks in 35..39){
+            return "D+"
+        }else if (marks in 30..34){
+            return "D"
+        }else if (marks in 25..29){
+            return "C+"
+        }
+        return "E"
+    }
 
 }
 
