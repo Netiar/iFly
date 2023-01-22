abb85fba868fb63e3554d0a22c9f6160a1c75d03
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-22 18:38:47 +0300

    get all scholars from the dummy database.

--- app/src/main/java/com/example/wingstofly/MainActivity.kt
+++ app/src/main/java/com/example/wingstofly/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.wingstofly
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import com.example.wingstofly.database.DataScholarManager
+import com.example.wingstofly.models.Scholar
 import com.example.wingstofly.repository.QuizRepository
 import com.example.wingstofly.viewmodel.QuizProviderFactory
 import com.example.wingstofly.viewmodel.QuizViewModel
 
 class MainActivity : AppCompatActivity() {
     lateinit var questViewModel: QuizViewModel
+    lateinit var scholars: ArrayList<Scholar>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,5 +19,8 @@ class MainActivity : AppCompatActivity() {
         val repo = QuizRepository()
         val provider = QuizProviderFactory(repo)
         questViewModel = ViewModelProvider(this, provider)[QuizViewModel::class.java]
+        val dm = DataScholarManager()
+        scholars = dm.scholars
+
     }
 }
