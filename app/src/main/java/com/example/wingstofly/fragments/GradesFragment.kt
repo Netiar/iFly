83d10ee408f88d312cd0280ec5bded4f3128c075
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-30 23:48:55 +0300

    bind the recycler view with the grades adapter.

---
+++ app/src/main/java/com/example/wingstofly/fragments/GradesFragment.kt
@@ -0,0 +1,45 @@
+package com.example.wingstofly.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.wingstofly.MainActivity
+import com.example.wingstofly.R
+import com.example.wingstofly.adapters.GradesRecAdapter
+import com.example.wingstofly.databinding.FragmentGradesBinding
+import com.example.wingstofly.models.Scholar
+import com.example.wingstofly.models.Subject
+import kotlinx.android.synthetic.main.fragment_grades.*
+
+class GradesFragment: Fragment(R.layout.fragment_grades) {
+    private lateinit var gradesRecAdapter: GradesRecAdapter
+    private lateinit var scholars: ArrayList<Scholar>
+    private lateinit var subjects: ArrayList<Subject>
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mainActivity =(activity as MainActivity)
+        scholars = mainActivity.scholars
+        subjects = mainActivity.subjects
+
+        for (scholar in scholars){
+            for (subject in subjects){
+                subject.score = 83
+                scholar.addSubject(subject)
+            }
+        }
+
+        gradesRecAdapter = GradesRecAdapter()
+        gradesRecAdapter.asyncListDiffer.submitList(scholars)
+        gradesRec.apply {
+            adapter = gradesRecAdapter
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+        }
+    }
+
+    private fun setUpRecyclerView() {
+
+    }
+}
