da081623c02cbed538332c2a56654fbf39882c0e
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-30 23:50:47 +0300

    create a variable to store all the subjects from the data manager.

--- app/src/main/java/com/example/wingstofly/MainActivity.kt
+++ app/src/main/java/com/example/wingstofly/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.example.wingstofly.database.DataScholarManager
+import com.example.wingstofly.database.Subjects
 import com.example.wingstofly.models.Scholar
+import com.example.wingstofly.models.Subject
 import com.example.wingstofly.repository.QuizRepository
 import com.example.wingstofly.viewmodel.QuizProviderFactory
 import com.example.wingstofly.viewmodel.QuizViewModel
@@ -12,6 +14,7 @@ import com.example.wingstofly.viewmodel.QuizViewModel
 class MainActivity : AppCompatActivity() {
     lateinit var questViewModel: QuizViewModel
     lateinit var scholars: ArrayList<Scholar>
+    lateinit var subjects: ArrayList<Subject>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,7 +23,9 @@ class MainActivity : AppCompatActivity() {
         val provider = QuizProviderFactory(repo)
         questViewModel = ViewModelProvider(this, provider)[QuizViewModel::class.java]
         val dm = DataScholarManager()
+        val subject = Subjects()
         scholars = dm.scholars
+        subjects = subject.allSubjects
 
     }
 }
