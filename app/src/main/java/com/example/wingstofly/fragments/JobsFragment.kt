d0c6caa792f8edadd333c272bc55fd726f2db53d
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-02-07 00:17:53 +0300

    feat: bind the job instances with the jobs adapter.

--- app/src/main/java/com/example/wingstofly/fragments/JobsFragment.kt
+++ app/src/main/java/com/example/wingstofly/fragments/JobsFragment.kt
@@ -4,19 +4,25 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.wingstofly.MainActivity
 import com.example.wingstofly.R
 import com.example.wingstofly.adapters.JobsRecView
+import com.example.wingstofly.models.Job
 import kotlinx.android.synthetic.main.activity_jobs.*
 import kotlinx.android.synthetic.main.activity_news.*
 import kotlinx.android.synthetic.main.activity_news.jobsRecView
 
 class JobsFragment: Fragment(R.layout.activity_jobs) {
     private lateinit var jobsAdapter: JobsRecView
+    private  var jobs = ArrayList<Job>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        jobs = (activity as MainActivity).jobs
+
         jobsAdapter = JobsRecView()
+        jobsAdapter.asyncList.submitList(jobs)
         setUpRecyclerView()
     }
 
