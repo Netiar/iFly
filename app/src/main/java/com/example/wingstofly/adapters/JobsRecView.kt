00b4fa156e48b87a02d72d794dfedf969aec6ca2
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-02-07 00:18:33 +0300

    feat: define a diff util to notify the adapter for data changes.

--- app/src/main/java/com/example/wingstofly/adapters/JobsRecView.kt
+++ app/src/main/java/com/example/wingstofly/adapters/JobsRecView.kt
@@ -3,22 +3,41 @@ package com.example.wingstofly.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wingstofly.R
+import com.example.wingstofly.databinding.ActivityJobRecviewBinding
+import com.example.wingstofly.models.Job
 
 class JobsRecView: RecyclerView.Adapter<JobsRecView.MyHolder>() {
-    inner class MyHolder(view: View): RecyclerView.ViewHolder(view)
+    inner class MyHolder(val bind: ActivityJobRecviewBinding): RecyclerView.ViewHolder(bind.root){
+        fun setData(job: Job){
+            bind.newsId.text = job.title
+            bind.newsDescription.text = "${job.description.subSequence(0, 150)}....."
+        }
+    }
+
+    private val diffUtil = object : DiffUtil.ItemCallback<Job>(){
+        override fun areItemsTheSame(oldItem: Job, newItem: Job): Boolean {
+            return  oldItem.jobUrl == newItem.jobUrl
+        }
+
+        override fun areContentsTheSame(oldItem: Job, newItem: Job): Boolean {
+            return  oldItem == newItem
+        }
+
+    }
+
+    val asyncList = AsyncListDiffer(this, diffUtil)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = MyHolder(
-        LayoutInflater.from(parent.context).inflate(
-            R.layout.activity_job_recview,
-            parent,
-            false
-        )
+        ActivityJobRecviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
     )
 
     override fun onBindViewHolder(holder: MyHolder, position: Int) {
+        holder.setData(asyncList.currentList[position])
     }
 
-    override fun getItemCount() = 10
+    override fun getItemCount() = asyncList.currentList.size
 }
