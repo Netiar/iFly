8ced6e6b26f80e9d0870c2cc4803c856a3c01ea3
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-02-17 16:53:27 +0300

    feat: bind the navigation header with scholar data.

--- app/src/main/java/com/example/wingstofly/fragments/HighSchoolFragment.kt
+++ app/src/main/java/com/example/wingstofly/fragments/HighSchoolFragment.kt
@@ -5,30 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.view.GravityCompat
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.navArgs
 import androidx.navigation.ui.setupWithNavController
 import com.example.wingstofly.MainActivity
 import com.example.wingstofly.R
-import com.example.wingstofly.databinding.FragmentMainBinding
+import com.example.wingstofly.databinding.FragmentHighSchoolBinding
 import com.example.wingstofly.viewmodel.QuizViewModel
 
 
-class MainFragment : Fragment(), View.OnClickListener {
+class HighSchoolFragment : Fragment(), View.OnClickListener {
 
-    private lateinit var bind:FragmentMainBinding
+    private lateinit var bind:FragmentHighSchoolBinding
     lateinit var viewModel: QuizViewModel
+    val args: HighSchoolFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        bind = FragmentMainBinding.inflate(layoutInflater)
+        bind = FragmentHighSchoolBinding.inflate(layoutInflater)
+        val scholar = args.scholar
 
         viewModel = (activity as MainActivity).questViewModel
 
         bind.image.setOnClickListener(this::onClick)
+        val scholarNames = scholar.name.split(" ")
+        bind.trvWelcome.text = "Welcome ${scholarNames[0]}"
+
+        // getting the navigation header components
+        val header = bind.navView.getHeaderView(0)
+        val scholarName = header.findViewById<TextView>(R.id.scholar_name)
+        val scholarNumber = header.findViewById<TextView>(R.id.pfNumber)
+
+        // Binding the header elements data.
+        scholarName.text = scholar.name
+        scholarNumber.text = scholar.pfNumber
+
 
         val navHostFragment = childFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
         bind.navView.bringToFront()
