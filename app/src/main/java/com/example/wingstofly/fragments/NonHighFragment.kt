34c9c3e7a97a0963031d1d50cd1b5ca9f65571ef
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-02-17 23:59:21 +0300

    feat: extend the onclick listener and bind the navigation view with the nav controller.

--- app/src/main/java/com/example/wingstofly/fragments/NonHighFragment.kt
+++ app/src/main/java/com/example/wingstofly/fragments/NonHighFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.findFragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -20,17 +21,24 @@ import com.example.wingstofly.databinding.FragmentNonHighBinding
  * Use the {@link NonHighFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-class NonHighFragment: Fragment() {
+class NonHighFragment: Fragment() , View.OnClickListener {
 
     private lateinit var bind:FragmentNonHighBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         bind = FragmentNonHighBinding.inflate(layoutInflater)
 
+        bind.image.setOnClickListener(this::onClick)
         val navHostFragment = childFragmentManager.findFragmentById(R.id.fragmentContainerView3) as NavHostFragment
         bind.navView.bringToFront()
         bind.navView.setupWithNavController(navHostFragment.navController)
 
         return bind.root
+    }
+
+    override fun onClick(p0: View?) {
+        if (p0 == bind.image){
+            bind.drawerLayout.openDrawer(GravityCompat.START)
+        }
     }
 
 }
