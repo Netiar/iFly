d3cc2e0b685f139dd7bc35b9e0ed491e35e4e9a0
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-02-21 05:24:07 +0300

    feat: initialize a primary and secondary constructor.

--- app/src/main/java/com/example/wingstofly/models/Message.kt
+++ app/src/main/java/com/example/wingstofly/models/Message.kt
@@ -1,7 +1,13 @@
 package com.example.wingstofly.models
 
-class Message {
+class Message : java.io.Serializable{
     var userUid: String? = null
     var description: String? = null
 
+    constructor(){}
+
+    constructor(userId: String, message: String){
+        this.userUid = userId
+        this.description = message
+    }
 }
