59223a560d8107cd62fc2da9845bf12e82700254
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-02-21 08:20:11 +0300

    feat: create an adapter to bind data to the bottom sheet.

---
+++ app/src/main/java/com/example/wingstofly/adapters/BottomSheetRecAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.wingstofly.adapters
+
+import android.content.Context
+import android.content.Intent
+import android.view.ContextThemeWrapper
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wingstofly.SingleActivity
+import com.example.wingstofly.databinding.ScholarBinding
+import com.example.wingstofly.models.Scholar
+
+class BottomSheetRecAdapter(val cont: Context): RecyclerView.Adapter<BottomSheetRecAdapter.MyHolder>() {
+    inner class MyHolder( val bind: ScholarBinding): RecyclerView.ViewHolder(bind.root){
+        fun setData(scholar: Scholar){
+            bind.scholarStatus.text = scholar.secondarySchool
+            bind.scholarName.text = scholar.name
+            bind.status.text = scholar.status
+
+            bind.root.setOnClickListener{
+                val intent = Intent(cont, SingleActivity::class.java)
+                intent.putExtra("layout", 3)
+                intent.putExtra("scholar", scholar)
+                cont.startActivity(intent)
+            }
+        }
+    }
+
+    private val diffUtil = object: DiffUtil.ItemCallback<Scholar>(){
+        override fun areItemsTheSame(oldItem: Scholar, newItem: Scholar): Boolean {
+            return oldItem.pfNumber == newItem.pfNumber
+        }
+
+        override fun areContentsTheSame(oldItem: Scholar, newItem: Scholar): Boolean {
+            return oldItem.equals(newItem)
+        }
+
+    }
+
+    val listDiffer = AsyncListDiffer(this, diffUtil)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = MyHolder(
+        ScholarBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    )
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+        holder.setData(listDiffer.currentList[position])
+    }
+
+    override fun getItemCount() = listDiffer.currentList.size
+}
