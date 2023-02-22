2e0e76392b1e1ee15d5b9c09d0a5bc93d2491241
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-02-22 04:44:44 +0300

    feat: inflate the recycler view root view and bind data to it.

--- app/src/main/java/com/example/wingstofly/adapters/NewsRecAdapter.kt
+++ app/src/main/java/com/example/wingstofly/adapters/NewsRecAdapter.kt
@@ -1,49 +1,66 @@
 package com.example.wingstofly.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wingstofly.R
-import com.example.wingstofly.databinding.FragmentEventsBinding
+import com.example.wingstofly.SingleActivity
+import com.example.wingstofly.models.Event
 import kotlinx.android.synthetic.main.activity_event_recview.view.*
 
 class NewsRecAdapter(var cont: Context): RecyclerView.Adapter<NewsRecAdapter.MyHolder>() {
 
-    inner class MyHolder(view: View): RecyclerView.ViewHolder(view){}
+    inner class MyHolder(val view: View): RecyclerView.ViewHolder(view){
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        if (parent.id == FragmentEventsBinding.inflate(LayoutInflater.from(cont)).eventsRecView.id ){
-            MyHolder(
-                LayoutInflater.from(parent.context).inflate(
-                    R.layout.activity_event_recview,
-                    parent,
-                    false
-                )
-
-            )
-
-        } else {
-            MyHolder(
-                LayoutInflater.from(parent.context).inflate(
-                    R.layout.activity_news_recview,
-                    parent,
-                    false
-                )
-
-            )
-        }
-
-    override fun onBindViewHolder(holder: MyHolder, position: Int) {
-        if (holder.itemView.id == FragmentEventsBinding.inflate(LayoutInflater.from(cont)).eventsRecView.id ){
-            holder.itemView.apply {
-                newsDescription.text = cont.getString(R.string.events)
-                newsId.text = cont.getString(R.string.eventId)
+        fun setClickListener(event: Event){
+            view.setOnClickListener{
+                val intent = Intent(cont, SingleActivity::class.java)
+                intent.putExtra("layout", 5)
+                intent.putExtra("event", event )
+                cont.startActivity(intent)
             }
         }
     }
 
-    override fun getItemCount() = 10
+    private val diffUtil = object: DiffUtil.ItemCallback<Event>() {
+
+        override fun areItemsTheSame(oldItem: Event, newItem: Event): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Event, newItem: Event): Boolean {
+            return oldItem.equals(newItem)
+        }
+
+    }
+
+    val asyncList = AsyncListDiffer(this, diffUtil)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        MyHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.activity_event_recview,
+                parent,
+                false
+            )
+
+        )
+
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+        val event = asyncList.currentList[position]
+        holder.itemView.apply {
+            newsDescription.text = event.description!!.subSequence(0, 50)
+            newsId.text = event.title
+        }
+        holder.setClickListener(event)
+    }
+
+    override fun getItemCount() = asyncList.currentList.size
 
 }
