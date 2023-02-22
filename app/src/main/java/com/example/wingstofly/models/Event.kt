133550f6526cceb1c4868305682d842e3a22c228
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-02-22 04:35:32 +0300

    fix: add more member variables.

--- app/src/main/java/com/example/wingstofly/models/Event.kt
+++ app/src/main/java/com/example/wingstofly/models/Event.kt
@@ -2,9 +2,11 @@ package com.example.wingstofly.models
 
 import java.util.Date
 
-class Event(var title:String){
+class Event(var title:String): java.io.Serializable{
+    var id: Int = 0
     var description:String? = null
     var venue:String? = null
     var date:Date? = null
+    var eventOwner: String? = null
 }
 
