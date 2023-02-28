cda015a7a936cd6162f7edf317570a8a6e786093
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-02-28 04:06:53 +0300

    feat: override the on stop method to dismiss the bottom sheet.

--- app/src/main/java/com/example/wingstofly/fragments/ChatFragment.kt
+++ app/src/main/java/com/example/wingstofly/fragments/ChatFragment.kt
@@ -1,14 +1,14 @@
 package com.example.wingstofly.fragments
 
+import android.content.SharedPreferences
 import android.content.res.Resources
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wingstofly.MainActivity
@@ -16,66 +16,86 @@ import com.example.wingstofly.R
 import com.example.wingstofly.adapters.BottomSheetRecAdapter
 import com.example.wingstofly.adapters.ChatRecHolder
 import com.example.wingstofly.adapters.ScholarsRecAdapter
-import com.example.wingstofly.databinding.BottomSheetBinding
 import com.example.wingstofly.databinding.FragmentChatBinding
-import com.example.wingstofly.databinding.ScholarBinding
+import com.example.wingstofly.models.Message
 import com.example.wingstofly.models.Scholar
+import com.example.wingstofly.utils.Constants
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import kotlinx.android.synthetic.main.fragment_single_job.view.*
+import com.google.firebase.database.*
 import kotlin.apply
 
 
 class ChatFragment : Fragment(), View.OnClickListener {
     private lateinit var bind:FragmentChatBinding
-    private lateinit var recBind: ScholarBinding
     private lateinit var suggestionAdapter: ScholarsRecAdapter
     private lateinit var chatAdapter: ChatRecHolder
-    private lateinit var navController: NavController
     private lateinit var bottomSheet: BottomSheetDialog
-    private lateinit var scholarsList: List<Scholar>
+    private lateinit var scholarsList: ArrayList<Scholar>
     private lateinit var scholarsAdapter: BottomSheetRecAdapter
+    private lateinit var pref: SharedPreferences
+    private lateinit var databaseReference: DatabaseReference
+
+    private var realScholar: Scholar? = null
+    private var pfNumber: String? = null
+    lateinit var hashMap: HashMap<Scholar, Message>
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         bind = FragmentChatBinding.inflate(inflater)
-        recBind = ScholarBinding.inflate(inflater, container, false)
-        navController = NavHostFragment.findNavController(this)
+        pref = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        databaseReference = FirebaseDatabase.getInstance().reference
         scholarsList = (activity as MainActivity).scholars
-        chatAdapter = ChatRecHolder()
         scholarsAdapter = BottomSheetRecAdapter(requireContext())
+        scholarsAdapter.listDiffer.submitList(scholarsList)
+
 
 
         bottomSheet = BottomSheetDialog(requireContext())
         createBottomSheetDialog(inflater, container)
-//        bottomSheet.window!!.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
 
         suggestionAdapter = ScholarsRecAdapter(requireContext())
 
         bind.fab.setOnClickListener(this::onClick)
 
-        val realScholar = Scholar("Charles Muvaka", "Student")
-        realScholar.apply {
-            origin = "Kericho Branch"
-            primarySchool = "Umoja primary school"
-            secondarySchool = "St Josephs Seminary Mwingi"
-            varsity = "The Technical University of Kenya"
+        pfNumber = pref.getString(Constants.PF_NUMBER, null)
+        getScholar(pfNumber)
+        hashMap = (activity as MainActivity).hashMap
+
+        if (hashMap.size != 0){
+            chatAdapter = (activity as MainActivity).chatAdapter
+            setUpRecyclerView()
+
+        }else{
+            bind.chats.text = "You have not chats."
         }
+
+
         val scholarSuggestion = ArrayList<Scholar>()
-        for (scholar in scholarsList){
-            if (scholar.origin == realScholar.origin){
-                scholarSuggestion.add(scholar)
-            }
-        }
+//        for (scholar in scholarsList){
+//            if (scholar.origin == realScholar!!.origin){
+//                scholarSuggestion.add(scholar)
+//            }
+//        }
 
         suggestionAdapter.listDiffer.submitList(scholarSuggestion)
 
-        setUpRecyclerView()
+
 
 
         return bind.root
     }
+
+    private fun getScholar(pfNumber: String?){
+        for (i in scholarsList.indices){
+            if (pfNumber.contentEquals(scholarsList[i].pfNumber)){
+                realScholar = scholarsList[i]
+            }
+            break
+        }
+    }
+
 
     private fun createBottomSheetDialog(inflater: LayoutInflater, container: ViewGroup?) {
         val view = inflater.inflate(R.layout.bottom_sheet, null, false)
@@ -99,10 +119,10 @@ class ChatFragment : Fragment(), View.OnClickListener {
     }
 
     private fun setUpRecyclerView() {
-        bind.topRecView.apply {
-            adapter = suggestionAdapter
-            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        }
+//        bind.topRecView.apply {
+//            adapter = suggestionAdapter
+//            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+//        }
 
         bind.chatRecView.apply {
             adapter = chatAdapter
@@ -117,5 +137,9 @@ class ChatFragment : Fragment(), View.OnClickListener {
         }
     }
 
+    override fun onStop() {
+        super.onStop()
+        bottomSheet.dismiss()
+    }
 
 }
