841a1cbda4b4d3df447e000765a9e91152106f3b
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-08 23:21:17 +0300

    ft: defining a fragment navigator extras instance to initialise animation effect.

--- app/src/main/java/com/example/wingstofly/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/wingstofly/fragments/LoginFragment.kt
@@ -6,9 +6,9 @@ import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.findFragment
+import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import com.example.wingstofly.MainActivity
 import com.example.wingstofly.R
@@ -70,10 +70,13 @@ class LoginFragment : Fragment(), View.OnClickListener {
                     val bundle = Bundle().apply {
                         putSerializable("scholar", scholar)
                     }
+                    //creating fragment navigator extras instance
+                    val transitionExtras = FragmentNavigatorExtras(bind.image to "fragment_image")
+
                     if (testNumber == "pf"){
-                        p0.findFragment<LoginFragment>().findNavController().navigate(R.id.action_loginFragment_to_highSchoolFragment, bundle)
+                             p0.findFragment<LoginFragment>().findNavController().navigate(R.id.action_loginFragment_to_highSchoolFragment, bundle, null, transitionExtras)
                     }else{
-                        p0.findFragment<LoginFragment>().findNavController().navigate(R.id.action_loginFragment_to_nonHighFragment, bundle)
+                        p0.findFragment<LoginFragment>().findNavController().navigate(R.id.action_loginFragment_to_nonHighFragment, bundle, null, transitionExtras)
                     }
                     break
                 }else{
