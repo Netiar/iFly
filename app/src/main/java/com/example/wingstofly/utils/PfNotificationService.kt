74b63f9c1518dae2b574824e6894646d222a279a
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-13 23:54:24 +0300

    ft: create an instance to create the local notification.

---
+++ app/src/main/java/com/example/wingstofly/utils/PfNotificationService.kt
@@ -0,0 +1,41 @@
+package com.example.wingstofly.utils
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.example.wingstofly.MainActivity
+import com.example.wingstofly.R
+import com.example.wingstofly.models.Scholar
+
+class PfNotificationService(private val cont: Context) {
+    private val notificationManager = cont.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    fun showNotification(scholar: Scholar){
+
+        //creating an intent to open the application
+        val intent = Intent(cont, MainActivity::class.java)
+
+        //creating a pending intent instance to initiate the intent
+        val pendingIntent = PendingIntent.getActivity(
+            cont,
+            1,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        //creating a notification instance
+        val notification = NotificationCompat.Builder(cont, PF_CHANNEL_ID)
+            .setSmallIcon(R.drawable.logo)
+            .setContentTitle("Scholar Number")
+            .setContentText("Hello ${scholar.name} here is your scholar number: ${scholar.pfNumber}")
+            .setContentIntent(pendingIntent).build()
+
+        notificationManager.notify(1, notification)
+
+    }
+    companion object{
+        const val PF_CHANNEL_ID = "pf_channel"
+    }
+}
