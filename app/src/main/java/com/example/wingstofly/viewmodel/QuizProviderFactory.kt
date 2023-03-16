6b13a173fe2bb76e983562fb42912cb4bce43f4a
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-16 23:15:08 +0300

    fix: add an argument to the constructor.

--- app/src/main/java/com/example/wingstofly/viewmodel/QuizProviderFactory.kt
+++ app/src/main/java/com/example/wingstofly/viewmodel/QuizProviderFactory.kt
@@ -4,9 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.wingstofly.repository.QuizRepository
 
-class QuizProviderFactory(private val quizRepository: QuizRepository): ViewModelProvider.Factory {
+class QuizProviderFactory(private val cat: String, private val quizRepository: QuizRepository): ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return QuizViewModel(quizRepository) as T
+        return QuizViewModel(cat, quizRepository) as T
     }
 }
