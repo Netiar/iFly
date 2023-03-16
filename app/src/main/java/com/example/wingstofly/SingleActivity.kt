4156a5fc8eeb6e425eae5ef914eca97f4c7b486c
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-16 23:15:51 +0300

    fix: get scholars data from the data manager.

--- app/src/main/java/com/example/wingstofly/SingleActivity.kt
+++ app/src/main/java/com/example/wingstofly/SingleActivity.kt
@@ -1,23 +1,35 @@
 package com.example.wingstofly
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.wingstofly.database.DataScholarManager
 import com.example.wingstofly.databinding.ActivitySingleBinding
 import com.example.wingstofly.fragments.*
-import com.example.wingstofly.models.Event
-import com.example.wingstofly.models.Job
-import com.example.wingstofly.models.Scholar
-import com.example.wingstofly.models.Upskill
+import com.example.wingstofly.models.*
+import com.example.wingstofly.repository.QuizRepository
+import com.example.wingstofly.utils.Constants
+import com.example.wingstofly.viewmodel.QuizProviderFactory
+import com.example.wingstofly.viewmodel.QuizViewModel
+import java.util.function.Predicate
 
 class SingleActivity : AppCompatActivity() {
     private lateinit var bind:ActivitySingleBinding
+    lateinit var questViewModel: QuizViewModel
+    lateinit var scholars: ArrayList<Scholar>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = ActivitySingleBinding.inflate(layoutInflater)
         setContentView(bind.root)
+
+        scholars = DataScholarManager().scholars
+
 
         val intent = intent
         val layout = intent.getIntExtra("layout", 0)
@@ -41,7 +53,21 @@ class SingleActivity : AppCompatActivity() {
             val fragment = UpskillFragment.newInstance(event)
             replaceFragment(fragment)
 
-        }else{
+        }else if (layout == 8){
+
+            val category = intent.getIntExtra("category", 16)
+            val categoryName = intent.getStringExtra("catName")
+            val triviaCategory = TriviaCategory(category, categoryName!!)
+
+            //initialising the quiz repository and provider
+            val repo = QuizRepository()
+            val provider = QuizProviderFactory(category.toString(), repo)
+            questViewModel = ViewModelProvider(this, provider)[QuizViewModel::class.java]
+            val fragment = QuestionsFragment.newInstance(triviaCategory)
+            replaceFragment(fragment)
+
+        }
+        else{
             val job = intent.getSerializableExtra("job") as Job
             val fragment = SingleJobFragment.newInstance(job)
             replaceFragment(fragment)
