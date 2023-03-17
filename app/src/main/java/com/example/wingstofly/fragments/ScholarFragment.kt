63a0dbd46b6a2182ee4d519af5ba1143e8a1fbdb
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-17 16:32:40 +0300

    ft: suggest scholars to the scholar.

--- app/src/main/java/com/example/wingstofly/fragments/ScholarFragment.kt
+++ app/src/main/java/com/example/wingstofly/fragments/ScholarFragment.kt
@@ -1,30 +1,29 @@
 package com.example.wingstofly.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.wingstofly.MainActivity
-import com.example.wingstofly.R
 import com.example.wingstofly.adapters.ScholarsRecAdapter
-import com.example.wingstofly.database.DataScholarManager
 import com.example.wingstofly.databinding.ChatBinding
-import com.example.wingstofly.databinding.FragmentChatBinding
 import com.example.wingstofly.databinding.FragmentScholarBinding
-import com.example.wingstofly.databinding.ScholarBinding
 import com.example.wingstofly.databinding.ScholarSuggestionBinding
 import com.example.wingstofly.models.Scholar
+import com.example.wingstofly.utils.Constants
 
 class ScholarFragment : Fragment() {
     private lateinit var bind:FragmentScholarBinding
     private lateinit var suggestionsBind:ScholarSuggestionBinding
     private lateinit var scholarBind: ChatBinding
     private lateinit var navController: NavController
+    private lateinit var pref: SharedPreferences
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,14 +36,31 @@ class ScholarFragment : Fragment() {
         bind = FragmentScholarBinding.inflate(inflater)
         scholarBind = ChatBinding.inflate(inflater, container, false)
         suggestionsBind = ScholarSuggestionBinding.inflate(inflater, container, false)
+        pref = PreferenceManager.getDefaultSharedPreferences(requireContext())
 
+        //getting the scholars pf number
+        val pfNumber = pref.getString(Constants.PF_NUMBER, null)
         val adapter = ScholarsRecAdapter(requireContext())
         val scholarsList = (activity as MainActivity).scholars
 
 
         adapter.listDiffer.submitList(scholarsList)
 
-        val scholarSuggestion = (activity as MainActivity).scholarSuggestion
+        val scholarSuggestion = ArrayList<Scholar>()
+
+        var realScholar: Scholar? = null
+        for (i in scholarsList.indices){
+            if (pfNumber.contentEquals(scholarsList[i].pfNumber)){
+                realScholar = scholarsList[i]
+                for (i in scholarsList.indices){
+                    if ((scholarsList[i].origin == realScholar.origin) || (scholarsList[i].secondarySchool == realScholar.secondarySchool)){
+                        scholarSuggestion.add(scholarsList[i])
+                    }
+                }
+            }
+
+        }
+        scholarSuggestion.remove(realScholar)
 
         val adapter1 = ScholarsRecAdapter(requireContext())
         adapter1.listDiffer.submitList(scholarSuggestion)
