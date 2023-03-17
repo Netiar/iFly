8a4bf24bda5833d13c98918a035ffcbd70d05b2a
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-17 16:31:33 +0300

    ft: remove unwanted methods.

--- app/src/main/java/com/example/wingstofly/MainActivity.kt
+++ app/src/main/java/com/example/wingstofly/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.example.wingstofly.database.*
 import com.example.wingstofly.models.*
@@ -16,19 +17,14 @@ import com.google.firebase.database.*
 class MainActivity : AppCompatActivity() {
     private lateinit var mAuth: DatabaseReference
     lateinit var questViewModel: QuizViewModel
-    lateinit var scholars: ArrayList<Scholar>
-    lateinit var subjects: ArrayList<Subject>
+    var scholars =  ArrayList<Scholar>()
     lateinit var jobs: ArrayList<Job>
     lateinit var events: ArrayList<Event>
     lateinit var scholarMessages: ArrayList<Message>
     var scholarSuggestion =  ArrayList<Scholar>()
     lateinit var schools: ArrayList<Upskill>
-    var realScholar: Scholar? = null
-
     private var pfNumber: String? = null
     private lateinit var pref: SharedPreferences
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,51 +44,25 @@ class MainActivity : AppCompatActivity() {
 
         //getting data from the data managers
         val dm = DataScholarManager()
-        val subject = Subjects()
         val jobManager = JobsDataManager()
         val eventManager = EventsDataManager()
-        scholarMessages = ArrayList()
         events = eventManager.allEvents
-        scholars = dm.scholars
-        subjects = subject.allSubjects
+
+        mAuth.child("user").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (data in snapshot.children){
+                    scholars.add(data.getValue(Scholar::class.java)!!)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@MainActivity,  error.message, Toast.LENGTH_LONG).show()
+            }
+
+        })
         jobs = jobManager.allJobs
         schools = UpSkillDataManager().schools
 
-        //getting the scholar using the app
-        realScholar = getScholar(pfNumber)
-
-        // assigning the scholar subjects marks
-        for (scholar in scholars){
-            for(subject in subjects){
-                when(subject.category){
-                    "Common" -> subject.score = 73
-                    "Humanities" -> subject.score = 86
-                    "Sciences" -> subject.score = 56
-                    else -> subject.score = 63
-                }
-
-                scholar.addSubject(subject)
-            }
-        }
-
-
     }
 
-    //defining a method to get the actual scholar
-    private fun getScholar(pfNumber: String?): Scholar{
-        var realScholar: Scholar? = null
-        for (i in scholars.indices){
-            if (pfNumber.contentEquals(scholars[i].pfNumber)){
-                realScholar = scholars[i]
-                for (i in scholars.indices){
-                    if ((scholars[i].origin == realScholar.origin) || (scholars[i].secondarySchool == realScholar.secondarySchool)){
-                        scholarSuggestion.add(scholars[i])
-                    }
-                }
-            }
-
-        }
-        scholarSuggestion.remove(realScholar)
-        return realScholar!!
-    }
 }
