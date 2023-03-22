117f1ca7b4288d4a65a72b17a2da2bce7c066322
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-22 12:40:49 +0300

    ft: define a uri member variable.

--- app/src/main/java/com/example/wingstofly/models/Upskill.kt
+++ app/src/main/java/com/example/wingstofly/models/Upskill.kt
@@ -3,4 +3,5 @@ package com.example.wingstofly.models
 class Upskill(var title:String): java.io.Serializable {
     var description:String? = null
     var type:String? = null
+    var uri: String? = null
 }
