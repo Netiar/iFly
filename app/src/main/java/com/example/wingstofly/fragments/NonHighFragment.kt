e57b793c4d5d170dd337672c4b2ed3f198539fda
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-23 15:34:25 +0300

    ft: change the status bar color.

--- app/src/main/java/com/example/wingstofly/fragments/NonHighFragment.kt
+++ app/src/main/java/com/example/wingstofly/fragments/NonHighFragment.kt
@@ -2,20 +2,19 @@ package com.example.wingstofly.fragments;
 
 import android.os.Bundle;
 import android.transition.TransitionInflater
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
-import androidx.fragment.app.findFragment
+import androidx.core.view.WindowCompat
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.navigation.ui.setupWithNavController
-
 import com.example.wingstofly.R;
 import com.example.wingstofly.databinding.FragmentNonHighBinding
 import com.example.wingstofly.models.Scholar
@@ -34,6 +33,24 @@ class NonHighFragment: Fragment() , View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //hiding the status bar
+        val window = requireActivity().window
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView)
+        // Configure the behavior of the hidden system bars.
+        windowInsetsController.isAppearanceLightStatusBars = true
+
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // finally change the color
+        window.statusBarColor = ContextCompat.getColor(requireActivity(),R.color.maroon3);
+
+
         //getting the animation transition from the login fragment
         val animationTransition = TransitionInflater.from(requireContext()).inflateTransition(
             android.R.transition.move
@@ -45,6 +62,8 @@ class NonHighFragment: Fragment() , View.OnClickListener {
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         bind = FragmentNonHighBinding.inflate(layoutInflater)
+
+        startAnimations()
 
         //getting the scholar from the graph
         scholar = args.scholar
@@ -66,6 +85,15 @@ class NonHighFragment: Fragment() , View.OnClickListener {
         bind.navView.setupWithNavController(navHostFragment.navController)
 
         return bind.root
+    }
+
+    private fun startAnimations() {
+        bind.image.startAnimation(AnimationUtils.loadAnimation(
+            requireContext(), R.anim.zoom_in
+        ))
+        bind.trvWelcome.startAnimation(AnimationUtils.loadAnimation(
+            requireContext(), R.anim.zoom_in
+        ))
     }
 
     override fun onClick(p0: View?) {
