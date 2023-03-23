90327e2601d72023ed3418add773654b7bb72044
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-23 15:31:50 +0300

    ft: initiate animations on the rec view.

--- app/src/main/java/com/example/wingstofly/fragments/FragmentEvents.kt
+++ app/src/main/java/com/example/wingstofly/fragments/FragmentEvents.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wingstofly.MainActivity
+import com.example.wingstofly.R
 import com.example.wingstofly.adapters.NewsRecAdapter
 import com.example.wingstofly.databinding.FragmentEventsBinding
 import com.example.wingstofly.models.Event
@@ -20,6 +22,7 @@ class FragmentEvents: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
        bind = FragmentEventsBinding.inflate(inflater)
         events = (activity as MainActivity).events
+        startAnimations()
 
         eventsAdapter = NewsRecAdapter(requireContext())
         eventsAdapter.asyncList.submitList(events)
@@ -27,6 +30,16 @@ class FragmentEvents: Fragment() {
 
         return bind.root
     }
+
+    private fun startAnimations() {
+        bind.upcoming.startAnimation(AnimationUtils.loadAnimation(
+            requireContext(), R.anim.zoom_in
+        ))
+        bind.eventsRecView.startAnimation(AnimationUtils.loadAnimation(
+            requireContext(), R.anim.zoom_in
+        ))
+    }
+
     private fun setUpRecyclerView(eventsRecView: RecyclerView) {
         eventsRecView.apply {
             adapter = eventsAdapter
