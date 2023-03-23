49be5ebed40c78661e14b4a89ab96e1a19805d3d
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-23 15:34:58 +0300

    ft: add animation to the views.

--- app/src/main/java/com/example/wingstofly/fragments/SplashFragment.kt
+++ app/src/main/java/com/example/wingstofly/fragments/SplashFragment.kt
@@ -6,6 +6,10 @@ import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.NavHostFragment
@@ -15,8 +19,41 @@ import com.example.wingstofly.databinding.FragmentSplashBinding
 class SplashFragment: Fragment() {
     private lateinit var bind: FragmentSplashBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //hiding the status bar
+        val window = requireActivity().window
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView)
+        // Configure the behavior of the hidden system bars.
+        windowInsetsController.isAppearanceLightStatusBars = true
+
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // finally change the color
+        window.statusBarColor = ContextCompat.getColor(requireActivity(),R.color.maroon2);
+
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         bind = FragmentSplashBinding.inflate(layoutInflater)
+
+        //adding a zoom in effect to the image
+        bind.image.startAnimation(AnimationUtils.loadAnimation(
+            requireContext(), R.anim.zoom_in
+        ))
+        bind.form.startAnimation(AnimationUtils.loadAnimation(
+            requireContext(), R.anim.zoom_in
+        ))
+        bind.slogan.startAnimation(AnimationUtils.loadAnimation(
+            requireContext(), R.anim.zoom_in
+        ))
 
         //creating navigation extras
         val extras = FragmentNavigatorExtras(bind.image to "login_image")
@@ -25,8 +62,30 @@ class SplashFragment: Fragment() {
         Handler(Looper.getMainLooper()).postDelayed(
             {
                 NavHostFragment.findNavController(this).navigate(R.id.action_splashFragment_to_loginFragment, null, null, extras)
-            }, 3000
+            }, 2000
         )
         return bind.root
     }
+
+    override fun onResume() {
+        super.onResume()
+        //hiding the status bar
+        val window = requireActivity().window
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView)
+        // Configure the behavior of the hidden system bars.
+        windowInsetsController.isAppearanceLightStatusBars = true
+
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // finally change the color
+        window.statusBarColor = ContextCompat.getColor(requireActivity(),R.color.maroon2);
+
+    }
+
 }
