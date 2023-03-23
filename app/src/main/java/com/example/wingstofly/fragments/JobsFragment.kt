43423f069ccf20b2f4fc3a9ed283293914d84fe8
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-23 15:33:06 +0300

    ft: initiate animations on the rec view.

--- app/src/main/java/com/example/wingstofly/fragments/JobsFragment.kt
+++ app/src/main/java/com/example/wingstofly/fragments/JobsFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wingstofly.MainActivity
+import com.example.wingstofly.R
 import com.example.wingstofly.adapters.JobsRecView
 import com.example.wingstofly.databinding.ActivityJobsBinding
 import com.example.wingstofly.models.Job
@@ -20,11 +22,19 @@ class JobsFragment: Fragment() {
         bind = ActivityJobsBinding.inflate(inflater)
 
         jobs = (activity as MainActivity).jobs
+        //start Animations
+        startAnimation()
 
         jobsAdapter = JobsRecView(requireContext())
         jobsAdapter.asyncList.submitList(jobs)
         setUpRecyclerView()
         return bind.root
+    }
+
+    private fun startAnimation() {
+        bind.jobsRecView.startAnimation(AnimationUtils.loadAnimation(
+            requireContext(), R.anim.zoom_in
+        ))
     }
 
     private fun setUpRecyclerView() {
