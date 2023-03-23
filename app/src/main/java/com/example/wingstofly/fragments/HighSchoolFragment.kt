bd27b6120b5761dc5984ad66c756c7b3a61a0087
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-23 15:32:52 +0300

    ft: initiate animations on the views.

--- app/src/main/java/com/example/wingstofly/fragments/HighSchoolFragment.kt
+++ app/src/main/java/com/example/wingstofly/fragments/HighSchoolFragment.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
+import androidx.core.view.WindowCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.navArgs
 import androidx.navigation.ui.setupWithNavController
@@ -26,6 +30,24 @@ class HighSchoolFragment : Fragment(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //hiding the status bar
+        val window = requireActivity().window
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView)
+        // Configure the behavior of the hidden system bars.
+        windowInsetsController.isAppearanceLightStatusBars = true
+
+
+// clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+        window.statusBarColor = ContextCompat.getColor(requireActivity(),R.color.maroon3);
+
+
         //getting  the animation transition
         val animationTransition = TransitionInflater.from(requireContext()).inflateTransition(
             android.R.transition.move
@@ -42,6 +64,8 @@ class HighSchoolFragment : Fragment(), View.OnClickListener {
         // Inflate the layout for this fragment
         bind = FragmentHighSchoolBinding.inflate(layoutInflater)
         val scholar = args.scholar
+
+        startAnimations()
 
         viewModel = (activity as MainActivity).questViewModel
 
@@ -65,9 +89,21 @@ class HighSchoolFragment : Fragment(), View.OnClickListener {
         return bind.root
     }
 
+    private fun startAnimations() {
+        bind.image.startAnimation(
+            AnimationUtils.loadAnimation(
+            requireContext(), R.anim.zoom_in
+        ))
+        bind.trvWelcome.startAnimation(
+            AnimationUtils.loadAnimation(
+            requireContext(), R.anim.zoom_in
+        ))
+    }
+
     override fun onClick(p0: View?) {
         if (p0 == bind.image){
             bind.drawerLayout.openDrawer(GravityCompat.START)
         }
     }
+
 }
