8043c5a2b4a0f280f09b5944a0ad35502b397861
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-29 00:00:00 +0300

    ft: bind scholar data to the views.

--- app/src/main/java/com/example/wingstofly/fragments/SharedFragment.kt
+++ app/src/main/java/com/example/wingstofly/fragments/SharedFragment.kt
@@ -5,31 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewbinding.ViewBinding
-import com.example.wingstofly.adapters.JobDetailsRecView
 import com.example.wingstofly.adapters.SubjectsRecAdapter
-import com.example.wingstofly.databinding.ChatBinding
 import com.example.wingstofly.databinding.FragmentSharedBinding
 import com.example.wingstofly.models.Scholar
 import com.example.wingstofly.utils.Constants
-import com.skydoves.transformationlayout.TransformationLayout
-import com.skydoves.transformationlayout.onTransformationEndContainer
+import kotlin.properties.Delegates
 
 
 class SharedFragment : Fragment() {
     private lateinit var bind: ViewBinding
     private lateinit var scholar: Scholar
+    private var number: Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val params = arguments?.getParcelable<TransformationLayout.Params>("TransformationParams")
-        onTransformationEndContainer(params)
-
         arguments?.let {
             scholar = it.getSerializable("scholar") as Scholar
+            number = it.getInt("number")
         }
     }
 
@@ -37,57 +34,52 @@ class SharedFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val scholarNumber = scholar.pfNumber
-        if (scholarNumber!!.subSequence(0, 2) == "pf") {
-            bind = FragmentSharedBinding.inflate(layoutInflater)
-            bind.root.transitionName = "myTransitionName"
+        bind = FragmentSharedBinding.inflate(layoutInflater)
+        bind.root.transitionName = "myTransitionName"
+        (bind as FragmentSharedBinding).currentGrades.text = "Form ${number + 1} grades"
 
-            (bind as FragmentSharedBinding).cutOff.text = "Scholar cut off: ${Constants.CUT_POINT}"
+        (bind as FragmentSharedBinding).cutOff.text = "Scholar cut off: ${Constants.CUT_POINT}"
 
-            (bind as FragmentSharedBinding).meanGrade.text = " Your mean grade is ${scholar.meanGrade}"
-            if (Constants.CUT_POINT_OFF_POINTS < scholar.meanAGP) {
-                (bind as FragmentSharedBinding).studentDeviation.text = "Congrats you are on the right track"
-            } else if (Constants.CUT_POINT_OFF_POINTS == scholar.meanAGP) {
-                (bind as FragmentSharedBinding).studentDeviation.text = "Hey, You are treading on a dangerous zone!!"
-            } else {
-                var deviation = scholar.calculateDeviation() * scholar.currentSubjects.size
+        (bind as FragmentSharedBinding).meanGrade.text =
+            "Form ${number + 1} mean grade: ${scholar.meanGrade[number.toString()]}"
+        if (scholar.pfNumber!!.subSequence(0, 2) == "pf") {
+            if (Constants.CUT_POINT_OFF_POINTS < scholar.meanAGP[number.toString()]!!) {
                 (bind as FragmentSharedBinding).studentDeviation.text =
-                    "Add more than $deviation marks in each subject to be on the safe side."
+                    "Congrats you are on the right track"
+            } else if (Constants.CUT_POINT_OFF_POINTS == scholar.meanAGP[number.toString()]) {
+                (bind as FragmentSharedBinding).studentDeviation.text =
+                    "Hey, You are treading on a dangerous zone!!"
+            } else {
+                var deviation =
+                    scholar.calculateDeviation(scholar.meanAGP[number.toString()]!!) * scholar.formGrades[number.toString()]!!.size
+                (bind as FragmentSharedBinding).studentDeviation.text =
+                    "$deviation marks required in each subject to beat the cut off."
             }
-            val adp = SubjectsRecAdapter()
-            adp.asyncList.submitList(scholar.currentSubjects)
-
-            (bind as FragmentSharedBinding).subRecView.apply {
-                adapter = adp
-                layoutManager = LinearLayoutManager(requireContext())
-            }
-        }else{
-            bind = ChatBinding.inflate(layoutInflater)
-            val list = arrayListOf<String>("The Technical University of Kenya - Business Information Technology", "Moringa School - Software Development.")
-            val list1 = arrayListOf<String>("Payments Officer - Equity Bank Ltd", "Casual Labourer - Osho.")
-            val adp = JobDetailsRecView(list)
-            val adp1 = JobDetailsRecView(list1)
-
-            (bind as ChatBinding).recView.apply {
-                adapter = adp
-                layoutManager = LinearLayoutManager(requireContext())
-            }
-
-            (bind as ChatBinding).view.apply {
-                adapter = adp1
-                layoutManager = LinearLayoutManager(requireContext())
-            }
+        } else {
+            (bind as FragmentSharedBinding).studentDeviation.text =
+                "Congrats you are on the right track"
         }
+        val adp = SubjectsRecAdapter()
+        adp.asyncList.submitList(scholar.formGrades[number.toString()])
+
+        (bind as FragmentSharedBinding).subRecView.apply {
+            adapter = adp
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+
+
 
         return bind.root
     }
+
     companion object {
 
         @JvmStatic
-        fun newInstance(param1: Scholar) =
+        fun newInstance(param1: Scholar, i: Int) =
             SharedFragment().apply {
                 arguments = Bundle().apply {
                     putSerializable("scholar", param1)
+                    putInt("number", i)
                 }
             }
     }
