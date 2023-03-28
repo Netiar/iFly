4d0b799786624d94b543ea19561650f77aa4c987
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-28 23:50:45 +0300

    ft: add a secondary constructor.

--- app/src/main/java/com/example/wingstofly/models/Event.kt
+++ app/src/main/java/com/example/wingstofly/models/Event.kt
@@ -2,11 +2,17 @@ package com.example.wingstofly.models
 
 import java.util.Date
 
-class Event(var title:String): java.io.Serializable{
+class Event: java.io.Serializable{
+    var title:String? = null
     var id: Int = 0
     var description:String? = null
     var venue:String? = null
     var date:Date? = null
     var eventOwner: String? = null
+
+    constructor(){}
+    constructor(title:String){
+        this.title = title
+    }
 }
 
