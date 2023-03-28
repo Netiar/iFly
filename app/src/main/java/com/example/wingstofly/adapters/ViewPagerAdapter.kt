bd85933284578ae753e6e646b4090d84ca36df6f
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-28 23:53:27 +0300

    ft: define an adapter to hold fragment instances.

---
+++ app/src/main/java/com/example/wingstofly/adapters/ViewPagerAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.wingstofly.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(private val list: ArrayList<Fragment>, fm: FragmentManager, lifecycle: Lifecycle): FragmentStateAdapter(fm, lifecycle) {
+    override fun getItemCount() = list.size
+    override fun createFragment(position: Int) = list[position]
+}
