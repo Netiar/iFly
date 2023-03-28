2327d2fd51731f4dcdb04e6c6db77a0a0beddb1f
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-28 23:52:41 +0300

    ft: add member variables.

--- app/src/main/java/com/example/wingstofly/models/Scholar.kt
+++ app/src/main/java/com/example/wingstofly/models/Scholar.kt
@@ -1,10 +1,15 @@
 package com.example.wingstofly.models
 
 import com.example.wingstofly.utils.Constants
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
-class Scholar: java.io.Serializable{
-    var name:String? = null; var status:String? = null
-    var pfNumber: String?  = null
+class Scholar : java.io.Serializable {
+    var scholarPfTest: String? = null
+    var name: String? = null;
+    var status: String? = null
+    var pfNumber: String? = null
     var primaryNumber: String? = null
     var otherNumber: String? = null
     var emailAddress: String? = null
@@ -14,31 +19,28 @@ class Scholar: java.io.Serializable{
     var primaryMarks: Int = 0
     var secondarySchool: String? = null
     var varsity: String? = null
-    var currentSubjects = ArrayList<Subject>()
-    var meanGrade: String? = null
-    var meanScore: Int = 0
-    var meanAGP: Int = 0
+    var meanGrade = HashMap<String, String>()
+    var meanScore = HashMap<String, Int>()
+    var meanAGP = HashMap<String, Int>()
     var coursesName: String? = null
     var varsityGrade: String? = null
-    var origin:String? = null
-    var tertiaryInstitutions = HashMap<String, String>()
+    var origin: String? = null
+    var tertiaryInstitutions = ArrayList<School>()
     var password: String? = null
     private val agpValues = HashMap<String, Int>()
+    var skillSet: String? = null
+    var leadershipRoles = ArrayList<Event>()
+    var workPlaces = ArrayList<Job>()
+    var formGrades = HashMap<String, ArrayList<Subject>>()
+    var form: Int = 4
 
-    constructor(){}
-    constructor(name:String, status:String){
+    constructor() {}
+    constructor(name: String, status: String) {
         this.name = name
         this.status = status
     }
 
-    fun addSubject(subject: Subject){
-        subject.grade = assignGrades(subject.score)
-        subject.agp = assignAGP(subject.grade!!)!!
-        currentSubjects.add(subject)
-        calculateMeanScore()
-    }
-
-    private fun assignGrades(marks: Int): String{
+    private fun assignGrades(marks: Int): String {
 
         agpValues["A"] = 12
         agpValues["A-"] = 11
@@ -53,85 +55,97 @@ class Scholar: java.io.Serializable{
         agpValues["D-"] = 2
         agpValues["E"] = 1
 
-        if(marks >= 79){
+        if (marks >= 79) {
             return "A"
-        }else if (marks in 75..78){
+        } else if (marks in 75..78) {
             return "A-"
-        }else if (marks in 70..74){
+        } else if (marks in 70..74) {
             return "B+"
-        }else if (marks in 65..69){
+        } else if (marks in 65..69) {
             return "B"
-        }else if (marks in 60..64){
+        } else if (marks in 60..64) {
             return "B-"
-        }else if (marks in 55..59){
+        } else if (marks in 55..59) {
             return "C+"
-        }else if (marks in 45..54){
+        } else if (marks in 45..54) {
             return "C"
-        }else if (marks in 40..44){
+        } else if (marks in 40..44) {
             return "C-"
-        }else if (marks in 35..39){
+        } else if (marks in 35..39) {
             return "D+"
-        }else if (marks in 30..34){
+        } else if (marks in 30..34) {
             return "D"
-        }else if (marks in 25..29){
+        } else if (marks in 25..29) {
             return "C+"
         }
         return "E"
     }
+
+    val list = ArrayList<Subject>()
+    fun addSubject(subject: Subject) {
+        subject.grade = assignGrades(subject.score)
+        subject.agp = assignAGP(subject.grade!!)!!
+        list.add(subject)
+        for (i in 0 until form) {
+            formGrades[i.toString()] = list
+        }
+        calculateMeanScore()
+    }
+
     private fun assignAGP(grade: String): Int? {
-        for (key in agpValues.keys){
-            if (key == grade){
+        for (key in agpValues.keys) {
+            if (key == grade) {
                 return agpValues[key]
             }
         }
         return 0
     }
-    private fun calculateMeanScore(){
-        var totalMarks = 0
-        val noOfSubjects:Int = currentSubjects.size
 
-        for (subject in currentSubjects){
-            totalMarks += subject.score!!
+    fun calculateMeanScore() {
+        var formScores = HashMap<Int, Int>()
+
+        for (i in 0 until formGrades.size) {
+            var totalMarks = 0
+            for (j in formGrades[i.toString()]!!.indices) {
+                totalMarks += formGrades[i.toString()]!![j].score
+            }
+            formScores[i] = totalMarks
         }
 
-        meanScore = totalMarks/noOfSubjects
-        scholarMeanGrade()
-        scholarMeanAGP()
-
+        for (i in 0 until form) {
+            val mean = formScores[i]!! / formGrades[i.toString()]!!.size
+            meanScore[i.toString()] = mean
+            meanGrade[i.toString()] = assignGrades(mean)
+            meanAGP[i.toString()] = assignAGP(meanGrade[i.toString()]!!)!!
+        }
     }
 
-    private fun scholarMeanGrade(){
-        meanGrade = assignGrades(meanScore)
-    }
 
-    private fun scholarMeanAGP(){
-        meanAGP = assignAGP(meanGrade!!)!!
-    }
-
-    fun calculateDeviation(): Int{
+    fun calculateDeviation(agp: Int): Int {
         var deviation = 0
-        deviation = Constants.CUT_POINT_OFF_POINTS - meanAGP
+        deviation = Constants.CUT_POINT_OFF_POINTS - agp
         return deviation
     }
 
 
 }
 
-class ScholarStatus{
+class ScholarStatus {
     var highSchool: String = "high-school"
     var campus: String = "University scholar"
     var preCampus: String = "A post graduate"
 }
 
-class Subject: java.io.Serializable{
-    var name: String? =null; var category: String? =null
+class Subject : java.io.Serializable {
+    var name: String? = null;
+    var category: String? = null
     var grade: String? = null;
     var agp: Int = 0
-    var score:Int = 0
+    var score: Int = 0
 
-    constructor(){}
-    constructor(name: String, category: String){
-      this.name = name
-      this.category = category
+    constructor() {}
+    constructor(name: String, category: String) {
+        this.name = name
+        this.category = category
     }
 }
