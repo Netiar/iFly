1831c643d7db132b0bd594b14fc50c9ef47afedb
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-04-02 17:49:25 +0300

    ft: define methods to change the scholars data.

--- app/src/main/java/com/example/wingstofly/fragments/NewsFragment.kt
+++ app/src/main/java/com/example/wingstofly/fragments/NewsFragment.kt
@@ -5,42 +5,225 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.wingstofly.adapters.NewsRecAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewbinding.ViewBinding
+import com.example.wingstofly.adapters.JobDetailsRecView
+import com.example.wingstofly.adapters.ResumeAdapter
 import com.example.wingstofly.databinding.ActivityNewsBinding
-import com.example.wingstofly.models.Event
+import com.example.wingstofly.databinding.ActivityNewsRecviewBinding
+import com.example.wingstofly.models.Scholar
+import com.example.wingstofly.models.School
 
-class NewsFragment: Fragment(), View.OnClickListener {
-    private lateinit var bind: ActivityNewsBinding
-    private lateinit var newsAdapter: NewsRecAdapter
-    private var event: Event? = null
+class NewsFragment : Fragment(), View.OnClickListener {
+    private lateinit var mainBind: ViewBinding
+    private var scholar: Scholar? = null
+    private var appearance: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            event = it.getSerializable("event") as Event?
+            scholar = it.getSerializable("event") as Scholar?
+            appearance = it.getString("appearance")
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        bind = ActivityNewsBinding.inflate(inflater)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
-        //binding event data to the fragment
-        bind.jobTitle.text = event!!.title
-        bind.scholarStatus.text = event!!.eventOwner
-        bind.description.text = event!!.description
+        if (appearance == "Portrait") {
+            mainBind = ActivityNewsRecviewBinding.inflate(inflater)
+            val bind = mainBind
 
-        bind.postingDate.text = event!!.date.toString()
-        bind.jobType.text = event!!.venue
+            //binding name and contact details
+            val names = scholar!!.name!!.split(" ")
+            val branch = scholar!!.origin!!.split(" ")
+            (bind as ActivityNewsRecviewBinding).name.text =
+                "${names!![0]} ${names[names.size - 1]}"
+            bind.box.text = scholar!!.primaryNumber
+            bind.place.text = "${branch[0]}, Kenya"
 
-        bind.back.setOnClickListener(this::onClick)
+            //binding education details
+            val school = School(scholar!!.primarySchool!!).apply {
+                course = "Junior studies Certification"
+                startYear = "08/01/2003"
+                endYear = "08/11/2010"
+            }
+            val school1 = School(scholar!!.secondarySchool!!).apply {
+                course = "High School Certification"
+                startYear = "08/02/2011"
+                endYear = "08/11/2014"
+            }
+            val educationAdapter = ResumeAdapter(requireContext())
+            educationAdapter.list = scholar!!.tertiaryInstitutions
+            educationAdapter.list!!.add(school1)
+            educationAdapter.list!!.add(school)
+            bind.educationRec.apply {
+                adapter = educationAdapter
+                layoutManager = LinearLayoutManager(requireContext())
 
-        return bind.root
+            }
+
+            //binding employment details
+            val employmentAdapter = ResumeAdapter(requireContext())
+            employmentAdapter.list1 = scholar!!.workPlaces
+            bind.employmentRec.apply {
+                adapter = employmentAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+            }
+
+
+            val list = ArrayList<String>()
+
+            //resume leadership roles
+            if (scholar!!.leadershipRoles.size != 0) {
+                for (role in scholar!!.leadershipRoles) {
+                    val roleName = role.title
+                    list.add(roleName!!)
+                }
+            } else {
+                val roleName = "1. Update your info"
+                val roleName1 = "2. Opportunities awaiting"
+                val roleName2 = "3. Opportunities awaiting"
+                list.add(roleName)
+                list.add(roleName1)
+                list.add(roleName2)
+            }
+            val leaderAdapter = JobDetailsRecView(list)
+            leaderAdapter.placeUsed = "Resume"
+            bind.leadershipRec.apply {
+                adapter = leaderAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+            }
+
+            //Generating dummy referees
+            val referee = School("John Doe").apply {
+                course = "Branch Growth and Development Manager"
+                startYear = "0768761610"
+                endYear = "muvakacharles@gmail.com"
+            }
+            val referee1 = School("Charles Muvaka").apply {
+                course = "Regional Manager Payments"
+                startYear = "0768761610"
+                endYear = "muvaka@gmail.com"
+            }
+            val referee2 = School("Mary Atieno").apply {
+                course = "General Manager"
+                startYear = "0768761610"
+                endYear = "charles@gmail.com"
+            }
+            val refList = arrayListOf(referee, referee1, referee2)
+            val refAdapter = ResumeAdapter(requireContext())
+            refAdapter.list = refList
+            refAdapter.placeUsed = "Resume"
+
+            bind.refereesRec.apply {
+                adapter = refAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+            }
+
+        } else {
+            mainBind = ActivityNewsBinding.inflate(layoutInflater)
+
+            //binding name and contact details
+            val names = scholar!!.name!!.split(" ")
+            val branch = scholar!!.origin!!.split(" ")
+            (mainBind as ActivityNewsBinding).name.text = "${names!![0]} ${names[names.size - 1]}"
+            (mainBind as ActivityNewsBinding).box.apply {
+                text = "Phone: ${scholar!!.primaryNumber}"
+            }
+            (mainBind as ActivityNewsBinding).place.text = "${branch[0]}, Kenya"
+
+            //binding education details
+            val school = School(scholar!!.primarySchool!!).apply {
+                course = "Junior studies Certification"
+                startYear = "08/01/2003"
+                endYear = "08/11/2010"
+            }
+            val school1 = School(scholar!!.secondarySchool!!).apply {
+                course = "High School Certification"
+                startYear = "08/02/2011"
+                endYear = "08/11/2014"
+            }
+            val educationAdapter = ResumeAdapter(requireContext())
+            educationAdapter.placeUsed = "Landscape"
+            educationAdapter.list = scholar!!.tertiaryInstitutions
+            educationAdapter.list!!.add(school1)
+            educationAdapter.list!!.add(school)
+            (mainBind as ActivityNewsBinding).educationRec.apply {
+                adapter = educationAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+
+            }
+
+            //binding employment details
+            val employmentAdapter = ResumeAdapter(requireContext())
+            employmentAdapter.list1 = scholar!!.workPlaces
+            (mainBind as ActivityNewsBinding).employmentRec.apply {
+                adapter = employmentAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+            }
+
+
+            val list = ArrayList<String>()
+
+            //resume leadership roles
+            if (scholar!!.leadershipRoles.size != 0) {
+                for (role in scholar!!.leadershipRoles) {
+                    val roleName = role.title
+                    list.add(roleName!!)
+                }
+            } else {
+                val roleName = "1. Update your info"
+                val roleName1 = "2. Opportunities awaiting"
+                val roleName2 = "3. Opportunities awaiting"
+                list.add(roleName)
+                list.add(roleName1)
+                list.add(roleName2)
+            }
+            val leaderAdapter = JobDetailsRecView(list)
+            leaderAdapter.placeUsed = "Resume"
+            (mainBind as ActivityNewsBinding).leadershipRec.apply {
+                adapter = leaderAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+            }
+
+            //Generating dummy referees
+            val referee = School("John Doe").apply {
+                course = "Branch Growth and Development Manager"
+                startYear = "0768761610"
+                endYear = "muvakacharles@gmail.com"
+            }
+            val referee1 = School("Charles Muvaka").apply {
+                course = "Regional Manager Payments"
+                startYear = "0768761610"
+                endYear = "muvaka@gmail.com"
+            }
+            val referee2 = School("Mary Atieno").apply {
+                course = "General Manager"
+                startYear = "0768761610"
+                endYear = "charles@gmail.com"
+            }
+            val refList = arrayListOf(referee, referee1, referee2)
+            val refAdapter = ResumeAdapter(requireContext())
+            refAdapter.list = refList
+            refAdapter.placeUsed = "Resume"
+
+            (mainBind as ActivityNewsBinding).refereesRec.apply {
+                adapter = refAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+            }
+        }
+        return mainBind.root
     }
 
-    companion object{
-        fun newInstance(event: Event): NewsFragment{
+    companion object {
+        fun newInstance(scholar: Scholar, appearance: String): NewsFragment {
             val args = Bundle().apply {
-                putSerializable("event", event)
+                putSerializable("event", scholar)
+                putString("appearance", appearance)
             }
             val fragment = NewsFragment()
             fragment.arguments = args
@@ -49,8 +232,6 @@ class NewsFragment: Fragment(), View.OnClickListener {
     }
 
     override fun onClick(p0: View?) {
-        if (p0 == bind.back){
-            requireActivity().finish()
-        }
+
     }
 }
