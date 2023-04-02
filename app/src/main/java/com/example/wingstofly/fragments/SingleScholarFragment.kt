ab1dc00e76f64f9eb434c9578b2cda5c07d86b2c
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-04-02 17:32:21 +0300

    ft: bind scholar data.

--- app/src/main/java/com/example/wingstofly/fragments/SingleScholarFragment.kt
+++ app/src/main/java/com/example/wingstofly/fragments/SingleScholarFragment.kt
@@ -30,16 +30,52 @@ class SingleScholarFragment : Fragment() {
 
 
         bind = FragmentSingleScholarBinding.inflate(inflater)
-        bind.primarySchool.text = "${param1!!.name!!.split(" ")[0]} is currently a student at ${param1!!.secondarySchool} after successfully completing her junior studies in ${param1!!.primarySchool}."
-        //bind.highGrade.text = "Mean-Grade: ${param1!!.meanGrade}"
-        //bind.highMarks.text = "Mean-AGP: ${param1!!.meanAGP.toString()}"
-        //bind.primaryMarks.text = "Total marks: ${param1!!.primaryMarks.toString()}"
-        //bind.primaryGrade.text = "Mean-Grade: ${param1!!.primaryMeanGrade}"
-        bind.branchOrigin.text = "${param1!!.name!!.split(" ")[0]} is a member of ${param1!!.origin} wings to fly community. Here is how you can reach her/him."
-        bind.phone.text = "Primary Number: ${param1!!.primaryNumber} \nOther Number: ${param1!!.otherNumber}"
 
-        if (param1!!.emailAddress?.isEmpty() == true){
-            bind.createEmail.visibility = View.VISIBLE
+        if(param1!!.pfNumber!!.subSequence(0,2) == "pf") {
+            bind.primarySchool.text = "${param1!!.name!!.split(" ")[0]} is currently a student at ${param1!!.secondarySchool} after successfully completing her junior studies in ${param1!!.primarySchool}."
+            bind.pMarks.text = "primary grade \n ${param1!!.primaryMeanGrade}"
+            bind.hGrade.text = "high school grade \n ${param1!!.primaryMeanGrade}"
+//            bind.deviation.text =
+  //              "current deviation \npoints: ${param1!!.calculateDeviation(param1!!.meanAGP[(param1!!.meanAGP.size - 1).toString()]!!)}"
+        }else{
+            bind.primarySchool.text = "${param1!!.name!!.split(" ")[0]} is a former student at ${param1!!.secondarySchool} after successfully completing her junior studies in ${param1!!.primarySchool}."
+            bind.pMarks.text = "primary grade \n ${param1!!.primaryMeanGrade}"
+            bind.hGrade.text = "high school grade \n ${param1!!.primaryMeanGrade}"
+           bind.deviation.text =
+            "Deviated By \npoints: ${param1!!.calculateDeviation(param1!!.meanAGP[(param1!!.meanAGP.size - 1).toString()]!!)}"
+
+        }
+        //binding the leadership roles
+        if(param1!!.leadershipRoles.size >= 3){
+            bind.leader1.text = "${param1!!.leadershipRoles[0].title} - ${param1!!.leadershipRoles[0].eventOwner}"
+            bind.leader2.text = "${param1!!.leadershipRoles[1].title} - ${param1!!.leadershipRoles[0].eventOwner}"
+            bind.leader3.text = "${param1!!.leadershipRoles[2].title} - ${param1!!.leadershipRoles[0].eventOwner}"
+        }else if (param1!!.leadershipRoles.size == 2){
+            bind.leader1.text = "${param1!!.leadershipRoles[0].title} - ${param1!!.leadershipRoles[0].eventOwner}"
+            bind.leader2.text = "${param1!!.leadershipRoles[1].title} - ${param1!!.leadershipRoles[0].eventOwner}"
+            bind.leader3.text = "Not updated"
+
+        }else if(param1!!.leadershipRoles.size == 1){
+            bind.leader1.text = "${param1!!.leadershipRoles[0].title} - ${param1!!.leadershipRoles[0].eventOwner}"
+            bind.leader2.text = "Not updated"
+            bind.leader3.text = "Not updated"
+        }else{
+            bind.leader1.text = "Not updated"
+            bind.leader2.text = "Not updated"
+            bind.leader3.text = "Not updated"
+        }
+
+        bind.branchOrigin.text = "${param1!!.name!!.split(" ")[0]} is a member of ${param1!!.origin} wings to fly community. Here is how you can reach her/him."
+       if(param1!!.primaryNumber.isNullOrBlank() && param1!!.otherNumber.isNullOrBlank() ){
+           bind.phone.text = "Primary Number: not updated \nOther Number: not updated"
+       }else if(param1!!.primaryNumber!!.isNotEmpty() && param1!!.otherNumber.isNullOrBlank()){
+           bind.phone.text = "Primary Number: ${param1!!.primaryNumber} \nOther Number: not updated"
+       }else{
+           bind.phone.text = "Primary Number: ${param1!!.primaryNumber} \nOther Number: ${param1!!.otherNumber}"
+       }
+
+        if (param1!!.emailAddress.isNullOrBlank()){
+            bind.email.text = "Email : not updated"
         }else{
             bind.email.text = "Email : ${param1!!.emailAddress}"
         }
