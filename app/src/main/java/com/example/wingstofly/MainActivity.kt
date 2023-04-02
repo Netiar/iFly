a70a3f7eecf1322168399d614c5e03791212134e
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-04-02 18:09:41 +0300

    ft: get data from the database.

--- app/src/main/java/com/example/wingstofly/MainActivity.kt
+++ app/src/main/java/com/example/wingstofly/MainActivity.kt
@@ -7,29 +7,27 @@ import android.preference.PreferenceManager
 import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
-import com.example.wingstofly.database.*
 import com.example.wingstofly.databinding.ActivityMainBinding
 import com.example.wingstofly.models.*
 import com.example.wingstofly.repository.QuizRepository
-import com.example.wingstofly.utils.Constants
 import com.example.wingstofly.viewmodel.QuizProviderFactory
 import com.example.wingstofly.viewmodel.QuizViewModel
 import com.google.firebase.database.*
 import com.skydoves.transformationlayout.onTransformationStartContainer
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var pref: SharedPreferences
     private lateinit var bind: ActivityMainBinding
     private lateinit var mAuth: DatabaseReference
     lateinit var questViewModel: QuizViewModel
     var scholars =  ArrayList<Scholar>()
-    lateinit var jobs: ArrayList<Job>
-    lateinit var events: ArrayList<Event>
-    lateinit var schools: ArrayList<Upskill>
-    private var pfNumber: String? = null
-    private lateinit var pref: SharedPreferences
+    var jobs =  ArrayList<Job>()
+    var events = ArrayList<Event>()
+    var schools =  ArrayList<Upskill>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        onTransformationStartContainer() // called before the super.onCreate method
+        onTransformationStartContainer() // initiating view transformation
         super.onCreate(savedInstanceState)
         bind = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bind.root)
@@ -38,25 +36,20 @@ class MainActivity : AppCompatActivity() {
         mAuth = FirebaseDatabase.getInstance().reference
         pref = PreferenceManager.getDefaultSharedPreferences(this)
 
-        //getting the scholars pf number
-        pfNumber = pref.getString(Constants.PF_NUMBER, null)
-
         //initialising the quiz repository and provider
         val repo = QuizRepository()
         val provider = QuizProviderFactory("", repo)
         questViewModel = ViewModelProvider(this, provider)[QuizViewModel::class.java]
 
-        //getting data from the data managers
-        val dm = DataScholarManager()
-        val jobManager = JobsDataManager()
-        val eventManager = EventsDataManager()
-        events = eventManager.allEvents
 
-        mAuth.child("user").addValueEventListener(object : ValueEventListener {
+
+        //getting the scholars from the database
+        mAuth.child("scholars").addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 for (data in snapshot.children){
                     scholars.add(data.getValue(Scholar::class.java)!!)
                 }
+
                 bind.progress.isVisible = false
             }
 
@@ -69,8 +62,48 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
-        jobs = jobManager.allJobs
-        schools = UpSkillDataManager().schools
+
+        //getting jobs from the database
+        mAuth.child("jobs").addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(job in snapshot.children){
+                    jobs.add(job.getValue(Job::class.java)!!)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@MainActivity, error.message, Toast.LENGTH_LONG).show()
+            }
+
+        })
+
+        //getting events from the database
+        mAuth.child("events").addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(event in snapshot.children){
+                    events.add(event.getValue(Event::class.java)!!)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@MainActivity, error.message, Toast.LENGTH_LONG).show()
+            }
+
+        })
+
+        //getting upskill schools from the database
+        mAuth.child("schools").addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(school in snapshot.children){
+                    schools.add(school.getValue(Upskill::class.java)!!)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@MainActivity, error.message, Toast.LENGTH_LONG).show()
+            }
+
+        })
 
     }
 
