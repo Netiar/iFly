411999a8898843b92962b8d8d47a133b83669925
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-04-07 02:46:44 +0300

    ft: add primary and secondary constructor.

--- app/src/main/java/com/example/wingstofly/models/Upskill.kt
+++ app/src/main/java/com/example/wingstofly/models/Upskill.kt
@@ -1,7 +1,13 @@
 package com.example.wingstofly.models
 
-class Upskill(var title:String): java.io.Serializable {
+class Upskill: java.io.Serializable {
+    var title:String? = null
     var description:String? = null
     var type:String? = null
     var uri: String? = null
+
+    constructor(title:String){
+        this.title = title
+    }
+    constructor()
 }
