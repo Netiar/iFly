ec9c5f0cbe15965ed8fbd480f57c5f10d72d4a8c
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-04-07 18:02:25 +0300

    ft: check network state.

--- app/src/main/java/com/example/wingstofly/SingleActivity.kt
+++ app/src/main/java/com/example/wingstofly/SingleActivity.kt
@@ -1,12 +1,17 @@
 package com.example.wingstofly
 
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.example.wingstofly.api.ConnectivityInterface
 import com.example.wingstofly.database.DataScholarManager
 import com.example.wingstofly.databinding.ActivitySingleBinding
 import com.example.wingstofly.fragments.*
+import com.example.wingstofly.models.NetworkConnectivityObserver
 import com.example.wingstofly.models.Scholar
 import com.example.wingstofly.models.TriviaCategory
 import com.example.wingstofly.models.Upskill
@@ -14,50 +19,53 @@ import com.example.wingstofly.repository.QuizRepository
 import com.example.wingstofly.viewmodel.QuizProviderFactory
 import com.example.wingstofly.viewmodel.QuizViewModel
 import com.skydoves.transformationlayout.TransformationAppCompatActivity
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class SingleActivity : TransformationAppCompatActivity() {
     private lateinit var bind:ActivitySingleBinding
     lateinit var questViewModel: QuizViewModel
     lateinit var scholars: ArrayList<Scholar>
-
+    private lateinit var contentManager: NetworkConnectivityObserver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = ActivitySingleBinding.inflate(layoutInflater)
         setContentView(bind.root)
+        contentManager = NetworkConnectivityObserver(applicationContext)
+        contentManager.observe().onEach {
+            if (it == ConnectivityInterface.States.Connected){
+                val intent = intent
+                val layout = intent.getIntExtra("layout", 0)
 
-        scholars = DataScholarManager().scholars
+                if (layout == 2 ){
+                    val scholar = intent.getSerializableExtra("scholar") as Scholar
+                    val fragment = ProfileFragment.newInstance(scholar)
+                    replaceFragment(fragment)
+                }else if(layout == 3){
+                    val scholar = intent.getSerializableExtra("scholar") as Scholar
+                    val fragment = MessagesFragment.newInstance(scholar)
+                    replaceFragment(fragment)
+                }else if (layout == 7){
+                    val event = intent.getSerializableExtra("school") as Upskill
+                    val fragment = UpskillFragment.newInstance(event)
+                    replaceFragment(fragment)
 
+                }else{
+                    val category = intent.getIntExtra("category", 16)
+                    val categoryName = intent.getStringExtra("catName")
+                    val triviaCategory = TriviaCategory(category, categoryName!!)
 
-        val intent = intent
-        val layout = intent.getIntExtra("layout", 0)
+                    //initialising the quiz repository and provider
+                    val repo = QuizRepository()
+                    val provider = QuizProviderFactory(category.toString(), repo)
+                    questViewModel = ViewModelProvider(this, provider)[QuizViewModel::class.java]
+                    val fragment = QuestionsFragment.newInstance(triviaCategory)
+                    replaceFragment(fragment)
 
-        if (layout == 2 ){
-            val scholar = intent.getSerializableExtra("scholar") as Scholar
-            val fragment = ProfileFragment.newInstance(scholar)
-            replaceFragment(fragment)
-        }else if(layout == 3){
-            val scholar = intent.getSerializableExtra("scholar") as Scholar
-            val fragment = MessagesFragment.newInstance(scholar)
-            replaceFragment(fragment)
-        }else if (layout == 7){
-            val event = intent.getSerializableExtra("school") as Upskill
-            val fragment = UpskillFragment.newInstance(event)
-            replaceFragment(fragment)
-
-        }else{
-            val category = intent.getIntExtra("category", 16)
-            val categoryName = intent.getStringExtra("catName")
-            val triviaCategory = TriviaCategory(category, categoryName!!)
-
-            //initialising the quiz repository and provider
-            val repo = QuizRepository()
-            val provider = QuizProviderFactory(category.toString(), repo)
-            questViewModel = ViewModelProvider(this, provider)[QuizViewModel::class.java]
-            val fragment = QuestionsFragment.newInstance(triviaCategory)
-            replaceFragment(fragment)
-
-        }
+                }
+            }
+        }.launchIn(lifecycleScope)
 
     }
 
