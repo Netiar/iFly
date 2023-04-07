67d7d6b7a410b4f516bdc7545c4fcf9eb839c32f
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-04-07 15:05:36 +0300

    minor changes

--- app/src/main/java/com/example/wingstofly/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/wingstofly/fragments/LoginFragment.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.findFragment
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
+import com.example.wingstofly.MainActivity
 import com.example.wingstofly.R
 import com.example.wingstofly.databinding.FragmentLoginBinding
 import com.example.wingstofly.models.Scholar
@@ -64,7 +65,7 @@ class LoginFragment : Fragment(), View.OnClickListener {
 
         startAnimations()
 
-        scholars = getScholars()
+        scholars = (activity as MainActivity).scholars
         pref = PreferenceManager.getDefaultSharedPreferences(requireContext())
         prefEditor = pref.edit()
         bind.submit.setOnClickListener(this::onClick)
